08b4a84e0b44add9f297e667ed7a95648791b49e
Mark <mark.arlou@itechart-group.com>
2021-11-19 13:04:09 +0300

    branch clearing

--- pages/basket_page.py
+++
@@ -1,7 +0,0 @@
-from .base_page import BasePage
-from .locators import BasketPageLocators
-
-class BasketPage(BasePage):
-   def basket_empty_message(self):
-       basketItems = (self.browser.find_element(*BasketPageLocators.BASKET_ITEMS)).text
-       assert basketItems == "Your basket is empty. Continue shopping"
--- test_basket_page.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from pages.basket_page import BasketPage
-
-base_link ="http://selenium1py.pythonanywhere.com/en-gb/basket/"
-
-def success_message():
-    page = BasketPage(browser,base_link)
-    page.open()
